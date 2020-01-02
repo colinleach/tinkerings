2de5518a9dc493303c99fc54bb01d91d1fd36112
colin <colin.leach@comcast.net>
2020-01-01 21:51:51 -0700

    Update observatories.py

--- uiprog/streamlit/observatories.py
+++ uiprog/streamlit/observatories.py
@@ -4,14 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from astropy import units as u
-from astropy.coordinates import SkyCoord, EarthLocation
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+
 from astropy.time import Time
 # from IPython.display import Image
 
 from urllib.parse import urlencode
 from urllib.request import urlretrieve
 
-sites = [
+southwest = [
  'Apache Point',
  'Catalina Observatory',
  'Discovery Channel Telescope',
@@ -28,6 +29,25 @@ sites = [
  'Whipple Observatory',
 ]
 
+chile = [
+    'ALMA',
+    'Cerro Paranal',
+    'Cerro Tololo',
+    'Gemini South',
+    'La Silla Observatory',
+    'Las Campanas Observatory',
+    # 'Paranal Observatory',
+]
+regions = {'SouthWestUS': southwest,
+            'Chile': chile}
+curr_region = st.sidebar.selectbox("Choose a region", list(regions.keys()), 0)
+st.sidebar.markdown("There is currently a [bug in streamlit](https://github.com/streamlit/streamlit/issues/475)")
+st.sidebar.markdown("This prevents the map scrolling automatically to the new region")
+st.sidebar.markdown("For now, use the mouse to pan and zoom - sorry")
+
+sites = regions[curr_region]
+curr_site = st.sidebar.radio("Sites in this region", sites)
+
 @st.cache
 def get_locations():
     return [EarthLocation.of_site(s) for s in sites]
@@ -36,10 +56,6 @@ locations = get_locations()
 
 @st.cache
 def make_locations_df():
-    locations = [EarthLocation.of_site(s) for s in sites]
-
-    # lats = [loc.lat.value for loc in locations]
-    # lons = [loc.lon.value for loc in locations]
     df = pd.DataFrame()
     df['name'] = sites
     df['lat'] = [loc.lat.value for loc in locations]
@@ -47,7 +63,9 @@ def make_locations_df():
     return df
 
 df = make_locations_df()
+
 # Adding code so we can have map default to the center of the data
+# Currently viewport only set on initial load: accepted as bug 
 midpoint = (np.average(df['lat']), np.average(df['lon']))
 
 st.deck_gl_chart(
@@ -75,5 +93,18 @@ st.deck_gl_chart(
                 "getColor": [0, 0, 200, 200],
                 "getSize": 15,
             },
-]
-        )
+        ]
+    )
+
+st.markdown(f"## {curr_site}")
+current_time = Time.now()
+observing_location = locations[sites.index(curr_site)]
+here = df.loc[df['name'] == curr_site].iloc[0]
+st.markdown(f"Latitude: {observing_location.lat:.2f}, Longitude: {observing_location.lon:.2f}")
+
+# st.markdown(f"")
+obj_name = 'm31'
+vega = SkyCoord.from_name(obj_name)
+aa_now = AltAz(location=observing_location, obstime=current_time)
+sky_coord = vega.transform_to(aa_now)
+st.markdown(f"{obj_name}: currently Alt = {sky_coord.alt:.2f}, Az = {sky_coord.az:.2f}")
